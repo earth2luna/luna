bad37d1149415e955c53a7c9fc6ef616c80537ea
laulyl <673348317@qq.com>
2017-06-21 23:36:55 +0800

    修改

--- luna-web/src/main/webapp/static/resources/custom/js/front.js
+++ luna-web/src/main/webapp/static/resources/custom/js/front.js
@@ -83,13 +83,8 @@ jQuery(function() {
 
 	$.scrollUp({
 		scrollName : "scrollUp",
-		topDistance : "300",
-		topSpeed : 300,
-		animation : "fade",
-		animationInSpeed : 200,
-		animationOutSpeed : 200,
-		scrollText : '<i class="fa fa-angle-up"></i>',
-		activeOverlay : !1
+		scrollFrom : "goTop",
+		scrollText : '<i class="fa fa-angle-up"></i>'
 	});
 
 	// view
